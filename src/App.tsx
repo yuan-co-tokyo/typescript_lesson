87561b4545292bb9d00d8cb84f081595630ce8fb
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-02-13 14:51:12 +0900

    4

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name = "hello";
+
+let nameChange = "hello";
+nameChange = "hello2"
+
+let userName = "Hello";
+let dummyNum = 2;
+let bool = true;
+
+let array1 = [true, false, true];
+let array2 = [0, 1, 2, true, "hello"];
+
+interface NAME{
+  first: string;
+  last: string;
+}
+
+interface NAME2{
+  first: string;
+  last: string | null;
+}
+
+let nameObj: NAME = {first:"ogura", last:"shion"};
+let nameObj2: NAME2 = {first:"ogura", last:null};
+
+const func1 = (x: number, y: number): number => {
+  return x + y;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
