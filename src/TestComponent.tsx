693487df9610fbe08d76627d6e46dc0b43aa8a0e
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-02-15 12:18:47 +0900

    16

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -12,12 +12,18 @@ interface User {
 const TestComponent: React.FC<Props> = (props) => {
     const [ count, setCount ] = useState<number | null>(null);
     const [ user, setUser ] = useState<User>({id:1, name: "dummy"});
+    const [ inputData, setInputData ] = useState("");
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement> ) =>
+        setInputData(e.target.value);
 
     return (
         <div>
             <h1>{ props.text }</h1>
             <h1>{count}</h1>
             <h3>{user.name}</h3>
+            <input type="text" value={inputData} onChange={handleInputChange} />
+            <h1>{inputData}</h1>
         </div>
     )
 }
