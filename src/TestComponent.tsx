6c69f4e420e3f03fb79e06cf112114acd6705f33
yuan.co.tokyo <yuan.co.tokyo@gmail.com>
2021-02-15 12:10:38 +0900

    15

--- src/TestComponent.tsx
+++ src/TestComponent.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 interface Props {
     text: string
 }
+interface User {
+    id: number;
+    name: string;
+}
 
 const TestComponent: React.FC<Props> = (props) => {
+    const [ count, setCount ] = useState<number | null>(null);
+    const [ user, setUser ] = useState<User>({id:1, name: "dummy"});
+
     return (
         <div>
             <h1>{ props.text }</h1>
+            <h1>{count}</h1>
+            <h3>{user.name}</h3>
         </div>
     )
 }
